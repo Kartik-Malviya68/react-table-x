7ea33a88cc2818afa48cdec992b7cf4f0be0ab1e
Kartik-Malviya68 <kartikpc68@gmail.com>
2025-09-08 14:29:25 +0530

    style: clean up code formatting and remove unnecessary div wrappers

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import Link from "next/link";
-
 export default function Home() {
   return (
-    <div>
-      <Link href="/products">Go to Products</Link>
-    </div>
+    <Link href="/products">Go to Products</Link>
   );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,7 +2,6 @@ import getProductList from '@/fetch/getProductList/getProductListService'
 import React from 'react'
 import ProductsTable from './Components/ProductsTable/ProductsTable'
 export default async function page() {
-
     const products = await getProductList()
     return (
         <div className='p-4 mt-8 container  mx-auto'><ProductsTable products={products} /></div>
